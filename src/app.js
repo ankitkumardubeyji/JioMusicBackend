de2a1788e5b2327723b5d772c5270f92490b86b6
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-04-09 18:35:26 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ app.use(cors({
     credentials:true 
 }))
 */
+app.use(cors({
+  origin: 'https://jio-music-frontend-7uzih0ed6-ankit-kumar-dubeys-projects.vercel.app'
+}));
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
