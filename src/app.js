129f0bca46c6c2cf938656a3c0fd073552764ec1
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-04-11 11:40:30 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,9 +10,8 @@ app.use(cors({
 }))
 */
 
-/*
-const allowedOrigins = ['https://jio-music-frontend.vercel.app/','https://jio-music-frontend-git-main-ankit-kumar-dubeys-projects.vercel.app/'
-                      ];
+
+const allowedOrigins = ['https://jio-music-frontend.vercel.app/'];
 app.use(
   cors({
     origin: (origin, callback) => {
@@ -29,7 +28,7 @@ app.use(
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }),
 );
-*/
+
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
