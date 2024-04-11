713bb29648fbf10484639714c876fd23f94dec70
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-04-11 17:48:01 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -127,8 +127,8 @@ const loginUser = asyncHandler(async(req,res,next)=>{
        secure:true 
     }
     return res.status(200)
-    .cookie("accessToken",accessToken,options)
-    .cookie("refreshToken",refreshToken,options)
+    .cookie("accessToken",accessToken)
+    .cookie("refreshToken",refreshToken)
     .json(new ApiResponse(200,{
         user:loggedInUser,
         accessToken,
