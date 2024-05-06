6bc091e93fa6e9aee2cd9aa34f4de19a92a749e3
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-05-06 09:40:11 +0530

    Update song.router.js

--- src/router/song.router.js
+++ src/router/song.router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { getAllSongs, getYourSongs, publishSong } from "../controllers/song.controller.js";
+import { getAllSongs, getYourSongs, publishSong,getEverySong } from "../controllers/song.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import {upload} from "../middleware/multer.middleware.js"
 
@@ -29,6 +29,7 @@ router
     );
 
  router.route("/").get(getAllSongs);   
+router.route("/every/song").get(getEverySong);
  router.route("/ys").get(getYourSongs);
 
 export default router;
